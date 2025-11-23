8cc733e727dcb6dfb4c2b1ab1682ebf4491c296d
Vasyl Linkin <linkinmvv@gmail.com>
2025-11-23 11:53:14 +0200

    fix

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ app.get('/notes', (req, res) => {
 
 app.get('/notes/:noteId', (req, res) => {
   const id_param = req.params.noteId;
-  res.status(200).json({ message: `"Retrieved note with ID: ${id_param}` });
+  res.status(200).json({ message: `Retrieved note with ID: ${id_param}` });
 });
 
 app.get('/test-error', () => {
