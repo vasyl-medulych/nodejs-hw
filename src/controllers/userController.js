d3a123df56e5485cfac998d4fde27cdfff78e94c
Vasyl Linkin <linkinmvv@gmail.com>
2025-12-21 22:40:08 +0200

    task complate

---
+++ src/controllers/userController.js
@@ -0,0 +1,17 @@
+import createHttpError from 'http-errors';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { User } from '../models/user.js';
+
+export const updateUserAvatar = async (req, res, next) => {
+  if (!req.file) {
+    throw createHttpError(400, 'No file');
+  }
+
+  const result = await saveFileToCloudinary(req.file.buffer, req.user._id);
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user._id,
+    { avatar: result.secure_url },
+    { new: true },
+  );
+  res.status(200).json({ url: updatedUser.avatar });
+};
--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import { errors } from 'celebrate';
 import cookieParser from 'cookie-parser';
 
@@ -20,6 +21,7 @@ app.use(logger);
 
 app.use(authRoutes);
 app.use(notesRoutes);
+app.use(userRoutes);
 
 app.use(notFoundHandler);
 app.use(errors());
